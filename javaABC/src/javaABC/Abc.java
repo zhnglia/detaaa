a1465ec41bcf021641ca1a41ec1ac0d2056af367
-foliage- <-foliage-@192.168.88.1>
2020-02-07 11:18:09 +0800

    测试，不用在意这些细节

---
+++ javaABC/src/javaABC/Abc.java
@@ -0,0 +1,21 @@
+package javaABC;
+
+class Abc {
+	static int m;
+	public static void fun() {
+		System.out.println(m);
+	}
+	public static void main(String[] args) {
+//		fun();
+		try { 
+			System.out.print(10 + 10 / 0); 
+		} catch(NullPointerException e1) { 
+			System.out.print("a"); 
+		} catch(RuntimeException e2) { 
+			System.out.print("b"); 
+		} finally { 
+			System.out.print("c"); 
+		} 
+
+	}
+}
