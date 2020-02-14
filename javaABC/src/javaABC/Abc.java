2d6fd9999de4d4350dedbd76fad0ad2d9b74b0cc
-foliage- <-foliage-@foliage>
2020-02-14 15:00:06 +0800

    ok

--- javaABC/src/javaABC/Abc.java
+++ javaABC/src/javaABC/Abc.java
@@ -1,5 +1,8 @@
 package javaABC;
 
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+
 class Abc {
 	static int m;
 	public static void fun() {
@@ -7,7 +10,7 @@ class Abc {
 	}
 	public static void main(String[] args) {
 		fun();
-		try { 
+		try {
 			System.out.print(10 + 10 / 0); 
 		} catch(NullPointerException e1) { 
 			System.out.print("a"); 
@@ -17,5 +20,30 @@ class Abc {
 			System.out.print("c"); 
 		} 
 		fun();
+		
+		ArrayBlockingQueue<Double> q = new ArrayBlockingQueue<Double>(10);
+		
+		//第一个线程线程，产生数据放入集合
+		new Thread() {
+			public void run() {
+				System.out.println("按回车产生数据");
+				new Scanner(System.in).nextLine();
+				double random = Math.random();
+				q.offer(random);
+			}
+		}.start();
+		
+		//第二个线程线程，从集合取数据，没有数据时会等待
+		new Thread() {
+			public void run() {
+				System.out.println("等待获取数据");
+				Double d;
+				try {
+					d = q.take();
+					System.out.println("已经获取数据"+d);
+				}catch (Exception e) {
+				}
+			}
+		}.start();
 	}
 }
